1ecbe3d93b37f2d2f8ca044a44db065a5db31afc
Michael Corrigan <1896158+ghost64@users.noreply.github.com>
2024-04-23 12:00:08 +0100

    Corrected branch name

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ghost64/scoop-apps:main"
+    [String]$upstream = "ghost64/scoop-apps:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
